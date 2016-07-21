25c4118385928ebffd4d509288a856191636f12c
Fabrizio Moscon <mosconfabrizio@gmail.com>
2016-07-21 23:45:19 +0100

    update lib

--- RNTwilioExample/src/services/endpoints.js
+++ RNTwilioExample/src/services/endpoints.js
@@ -6,7 +6,7 @@ import Config from 'react-native-config'
 
 export default {
     calls: {
-        make:  Config.API_URL + 'calls',
-        token: Config.API_URL + 'token',
+        make:  Config.API_URL + 'calls/',
+        token: Config.API_URL + 'token/',
     }
 }
